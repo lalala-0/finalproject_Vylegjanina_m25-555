7cfce0068d48fa1ff0dc2bee7fc1be6022ab65a7
lalala-0 <vylegzhanina.alena@yandex.ru>
2025-11-05 22:07:02 +0300

    feat: создан класс Wallet

--- valutatrade_hub/core/models.py
+++ valutatrade_hub/core/models.py
@@ -64,3 +64,45 @@ class User:
         self._salt = self._generate_salt()
         self._hashed_password = self._hash_password(new_password)
 
+
+class Wallet:
+    def __init__(self, currency_code: str, balance: float = 0.0):
+        if not isinstance(currency_code, str) or not currency_code:
+            raise ValueError("Код валюты должен быть непустой строкой")
+        if not isinstance(balance, (int, float)) or balance < 0:
+            raise ValueError("Баланс должен быть числом не меньше 0")
+
+        self.currency_code = currency_code.upper()
+        self._balance = float(balance)
+
+    @property
+    def balance(self) -> float:
+        return self._balance
+
+    @balance.setter
+    def balance(self, value: float):
+        if not isinstance(value, (int, float)):
+            raise TypeError("Баланс должен быть числом")
+        if value < 0:
+            raise ValueError("Баланс не может быть отрицательным")
+        self._balance = float(value)
+
+    def deposit(self, amount: float):
+        """Пополняет баланс на указанную сумму."""
+        if not isinstance(amount, (int, float)):
+            raise TypeError("Сумма пополнения должна быть числом")
+        if amount <= 0:
+            raise ValueError("Сумма пополнения должна быть положительной")
+        self._balance += float(amount)
+
+    def withdraw(self, amount: float):
+        """Снимает средства с баланса, если хватает средств."""
+        if not isinstance(amount, (int, float)):
+            raise TypeError("Сумма снятия должна быть числом")
+        if amount <= 0:
+            raise ValueError("Сумма снятия должна быть положительной")
+        if amount > self._balance:
+            raise ValueError("Недостаточно средств на балансе")
+
+
+
